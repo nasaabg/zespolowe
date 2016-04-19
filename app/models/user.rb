dd4ea0c90be42e58ae3fd10ddfd60d72b1f226f5
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-04-19 23:57:26 +0200

    remove omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-     :rememberable, :trackable, :validatable, :omniauthable
+     :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
