f15e0088be6c28b345c4235a386d39ff0ddd4522
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-04-19 23:26:40 +0200

    Add secrets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api, defaults: { format: :json } do
+     scope module: :v1 do
+     end
+   end
 end
