ba960bb2e4f868e6f98294a8f06b2d3e2a344e5d
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-05-28 12:43:35 +0200

    Add api doc

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,16 +2,42 @@ class Api::V1::CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
   before_action :set_commentable
 
+  api!
   def index
     comments = @commentable.comments
     respond_with comments
   end
 
+  api!
   def show
     comment = @commentable.comments.find(params[:id])
     respond_with comment
   end
 
+  api!
+  description <<-EOS
+  === Wymagana autentykacja
+  === Success response:
+    {
+      "id": 4,
+      "title": "",
+      "comment": "My new comment",
+      "commentable_id": 9,
+      "commentable_type": "Question",
+      "user_id": null,
+      "role": "comments",
+      "created_at": "2016-05-28T10:38:51.672Z",
+      "updated_at": "2016-05-28T10:38:51.672Z"
+    }
+  EOS
+  example <<-EOS
+  Example json to create Question
+  {
+    "comment":{
+        "comment": "My new comment"
+    }
+  }
+  EOS
   def create
     comment = @commentable.comments.new(comment_params)
     comment.user = current_user
