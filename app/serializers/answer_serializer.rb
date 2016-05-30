2d523337d6ec70905c19de9a3c0a8669233c694d
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-05-30 09:37:26 +0200

    Add user to answer json

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,4 +1,6 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :body, :is_accepted,
-    :created_at, :user_id, :question_id, :counter
+    :created_at, :question_id, :counter
+
+  has_one :user
 end
