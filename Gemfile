ce3eebc98b29c176478602697075d83d83f0e1eb
Jan Kurzydlo <janek.kurzydlo@gmail.com>
2016-06-11 20:30:57 +0200

    Add Faker and Seeds

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'acts_as_commentable'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
@@ -34,6 +35,7 @@ gem 'rails_admin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'faker', '1.6.3'
   gem 'byebug'
   gem 'better_errors'
   gem 'binding_of_caller'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+password = Faker::Internet.password
+user1 = User.create!(email: Faker::Internet.email, password: password, password_confirmation: password, confirmed_at: Time.now, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialization: "Programmer", gender: "male")
+user2 = User.create!(email: Faker::Internet.email, password: password, password_confirmation: password, confirmed_at: Time.now, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialization: "Programmer", gender: "male")
+puts "Users created"
+
+Question.destroy_all
+question1 = Question.create!(title: Faker::Name.title, description: Faker::Hipster.paragraph(2), user: user1, tag_list: ["awesome", "ruby"])
+question2 = Question.create!(title: Faker::Name.title, description: Faker::Hipster.paragraph(2), user: user2, tag_list: ["yolo", "project"])
+puts "Questions created"
+
+QuestionPoint.destroy_all
+QuestionPoint.create!(user: user2, question: question1)
+QuestionPoint.create!(user: user1, question: question2)
+puts "Question points"
+
+Answer.destroy_all
+Answer.create!(question: question1, user: user2, is_accepted: true, body: Faker::Hipster.paragraph(2))
+Answer.create!(question: question2, user: user1, is_accepted: true, body: Faker::Hipster.paragraph(2))
+answer1 = Answer.create!(question: question1, user: user2, is_accepted: false)
+puts "Answers created"
+
+Comment.destroy_all
+answer1.comments.create!(title: Faker::Name.title, comment: Faker::Hipster.paragraph(2), user: user1)
+answer1.comments.create!(title: Faker::Name.title, comment: Faker::Hipster.paragraph(2), user: user2)
+puts "Comments created"
